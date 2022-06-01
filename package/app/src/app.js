49a8538308297e1c3ef9110fc98581507574f4f6
Jose Luis Campaña <macpanda@gmail.com>
2022-06-01 22:48:37 +0200

    skeleton

---
+++ package/app/src/app.js
@@ -0,0 +1,18 @@
+import React, { useState, useCallback } from 'react'
+// import Editor from './editor'
+// import Preview from './preview'
+// import './app.css'
+
+const App = () => {
+
+
+  return (
+    <div className='app'>
+      <div>
+        <h1>test</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
