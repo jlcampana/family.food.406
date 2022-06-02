a3db6a719daa8248499a8904e058d0fc434029eb
Jose Luis Campaña <macpanda@gmail.com>
2022-06-02 21:16:27 +0200

    feat: not working

--- package/app/src/app.js
+++ package/app/src/app.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useCallback } from 'react'
 // import Editor from './editor'
 // import Preview from './preview'
@@ -5,8 +6,8 @@ import React, { useState, useCallback } from 'react'
 
 const App = () => {
 
-
   return (
+
     <div className='app'>
       <div>
         <h1>test</h1>
--- package/app/src/index.js
+++ package/app/src/index.js
@@ -1,10 +1,11 @@
-
+import { ChakraProvider } from '@chakra-ui/react'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './main.css'
 import App from './app'
 
-ReactDOM.render(
-  <App/>,
+ReactDOM.render(<ChakraProvider>
+  <App />
+</ChakraProvider>,
   document.getElementById('root')
 );
